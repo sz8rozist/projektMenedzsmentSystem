7ca7dfc357925adfdaf324b0466a0995f1930502
Rózsa István <ristvan98@gmail.com>
2023-07-21 00:13:17 +0200

    Javítás

--- frontend/projekt_menedzsment/src/app/signin/signin.component.ts
+++ frontend/projekt_menedzsment/src/app/signin/signin.component.ts
@@ -1,58 +1,50 @@
-
-
 import { AuthService } from '../service/auth.service';
 import { User } from '../model/User';
-import {FormGroup, Validators, FormControl} from '@angular/forms';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
-
   signinForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
+    password: new FormControl('', [Validators.required]),
   });
 
-  errorMsg : string = "";
+  errorMsg: string = '';
 
-  constructor(
-    private authService: AuthService,
-    private router: Router
-    ){}
+  constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit(){
+  ngOnInit() {
     const authenticated = this.authService.isAuthenticated();
-    if(authenticated){
-      this.router.navigate(["/home"]);
+    if (authenticated) {
+      this.router.navigate(['/home']);
     }
   }
 
-  onSignin(){
+  onSignin() {
     const user: User = {
       username: this.signinForm.get('username')?.value as string,
       password: this.signinForm.get('password')?.value as string,
-    }
+    };
     this.authService.signin(user).subscribe(
       (response) => {
         console.log(response);
-        if(response.token != null){
-          localStorage.setItem("token", response.token);
-          this.router.navigate(["/home"]);
-        }else{
+        if (response.token != null) {
+          localStorage.setItem('token', response.token);
+          this.router.navigate(['/home']);
+        } else {
           //Sikertelen login
-            this.errorMsg = "Sikertelen bejelentkezés";
+          this.errorMsg = 'Sikertelen bejelentkezés';
         }
       },
-      error => {
+      (error) => {
         console.log(error);
       }
-    )
+    );
   }
-
 }
